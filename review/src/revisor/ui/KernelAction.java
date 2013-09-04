627be7bd6e25b1bb805f965f286365f8c6acd5fb
fillipemanoel@gmail.com <fillipemanoel@gmail.com@ad98d650-ed9f-cb44-ca82-9d8feac0cd92>
2013-09-04 21:06:57 +0000

    Imports organized

--- review/src/revisor/ui/KernelAction.java
+++ review/src/revisor/ui/KernelAction.java
@@ -2,30 +2,16 @@ package revisor.ui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Set;
 
 import org.protege.editor.owl.model.OWLModelManager;
 import org.protege.editor.owl.model.parser.OWLParseException;
 import org.protege.editor.owl.ui.clsdescriptioneditor.ExpressionEditor;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.io.OWLOntologyCreationIOException;
-import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClassAxiom;
 import org.semanticweb.owlapi.model.OWLException;
 import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyFormat;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
-
-import revisor.ui.PostulateButton;
-import revisor.ui.PostulateGroup;
-import revisor.ui.RevisorAbstractView;
 
 /**
  * Authors: Márcio Moretto Ribeiro and Fillipe Resina
